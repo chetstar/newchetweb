c1221a43e8094e3c1631ef9320ffefd8ce1cdd54
chet star <cmeinzer@acbhcs.org>
2014-04-23 13:22:45 -0700

    new folder structure and gunicorn

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from flask_bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
+# from flask.ext.sqlalchemy import SQLAlchemy
+
+# initialization
+app = Flask(__name__)
+app.config.update(
+    DEBUG = True,
+)
+app.config.from_object('config')
+#now i can access config vars via app.config['VAR_Name']
+Bootstrap(app)
+db = SQLAlchemy(app)
+from views import *
+from models import *
+# launch
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
---
+++ app/views.py
@@ -0,0 +1,34 @@
+from app import app
+from flask import Flask, render_template, send_from_directory, request, flash,redirect,url_for
+from forms import ContactForm
+
+app.secret_key = 'development key'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return redirect(url_for('index'))
+        else:
+            return 'Form posted.'
+    elif request.method == 'GET':
+       return render_template('index.html', form=form)
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == 'POST':
+        if form.validate() == False:
+            flash('All fields are required.')
+            return redirect(url_for('index'))
+        else:
+            return 'Form posted.'
+    elif request.method == 'GET':
+       return render_template('index.html', form=form)
